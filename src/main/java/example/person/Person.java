2f04e84cfaab7a82870b3eeb538e4d9d93689572
yi cheng <i.albert.cheng@gmail.com>
2020-04-26 01:11:24 +0100

    lombok

--- src/main/java/example/person/Person.java
+++ src/main/java/example/person/Person.java
@@ -5,8 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Person {
 
   @Id
@@ -15,51 +20,8 @@ public class Person {
   private String firstName;
   private String lastName;
 
-  protected Person() {
-  }
-
   public Person(String firstName, String lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-      if (this == o) {
-          return true;
-      }
-      if (o == null || getClass() != o.getClass()) {
-          return false;
-      }
-    Person person = (Person) o;
-    return id == person.id &&
-        Objects.equals(firstName, person.firstName) &&
-        Objects.equals(lastName, person.lastName);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, firstName, lastName);
-  }
-
-  @Override
-  public String toString() {
-    return "Person{" +
-        "id='" + id + '\'' +
-        ", firstName='" + firstName + '\'' +
-        ", lastName='" + lastName + '\'' +
-        '}';
   }
 }
